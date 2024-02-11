e0177c6e13ca671a00ff299d04aa27566b071f98
hafabdulsami <abdulsami699@gmail.com>
2024-02-11 05:03:46 +0500

    error in the edit controller

--- routes/CategoryRoute.js
+++ routes/CategoryRoute.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const {upload} = require("../Upload");
+const { upload } = require("../Upload");
 const {
   createCategory,
   getCategory,
   editCategory,
-  
 } = require("../controllers/CategoryController");
 
-router.post("/createCategory",upload.array("images",2),createCategory);
+router.post("/createCategory", upload.array("images", 2), createCategory);
 router.get("/getCategory", getCategory);
-router.put("/editCategory", editCategory);
+router.put("/editCategory", upload.array("images",5), editCategory);
 //router.get("/deleteCategory", deleteCategory);
 module.exports = router;
